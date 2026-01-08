265510fc30064d45df7e7bd925b30641c7a24874
Kevin Garcia <ermianrazor@gmail.com>
2026-01-08 00:32:51 -0500

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@fobos-chat/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
