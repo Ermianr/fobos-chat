92f5114e67137d94e029f5603d6440a481f9ad56
Kevin Garcia <ermianrazor@gmail.com>
2026-01-08 22:38:29 -0500

    feat: initial commit

---
+++ commitlint.config.ts
@@ -0,0 +1,7 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [2, "always", "lower-case"],
+    "subject-max-length": [2, "always", 50],
+  },
+};
